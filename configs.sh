3a79f74eaf803ed70418aac8f9a6542316208142
jasonzzzzzzz <53132593+jasonzzzzzzz@users.noreply.github.com>
2019-10-28 20:18:51 -0400

    first commit

---
+++ configs.sh
@@ -0,0 +1,9 @@
+# Set this to point to the top level of the TailBench data directory
+DATA_ROOT=/topleveldatadir
+
+# Set this to point to the top level installation directory of the Java
+# Development Kit. Only needed for Specjbb
+ JDK_PATH=/pathtojdk
+
+# This location is used by applications to store scratch data during execution.
+SCRATCH_DIR=/pathtoscratch
